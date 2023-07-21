f1a4d7ab8e6d01b1c2ffe72d73073e74bda17776
ezvel <lvelezezequiel7@gmail.com>
2023-07-21 11:50:33 -0300

    Init commit

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
-const $btnEnviar = document.getElementById("enviar");
+const $btnEnviar = document.getElementById("cargar");
+const $btnModificar = document.getElementById("modificar");
 
 //Al recargar la página mostrame todos mis contactos
+
 document.addEventListener("DOMContentLoaded", () => {
-    mostrarTodosLosContactos();
+    const $botonModificar = document.getElementById("modificar");
+    $botonModificar.disabled = true;
+    mostrarContactos();
 });
 
 //Al hacer click en enviar: 1)Validar los datos, 2)Agregar el contacto
-$btnEnviar.addEventListener("submit", () => {
+$btnEnviar.addEventListener("click", (e) => {
     let nombre = document.getElementById("nom").value;
     let apellido = document.getElementById("ape").value;
     let telefono = document.getElementById("tel").value;
@@ -23,8 +27,47 @@ $btnEnviar.addEventListener("submit", () => {
     
     //const hayErrores = validarFormulario(datos);
 
+    
+    const hayErrores = false;
+    
+    
     if(!hayErrores) {
-        enviarDatos(datos);
+        agregarContacto(datos);
     }
 
+    e.preventDefault();
+
 });
+
+
+//Modificar contacto
+$btnModificar.addEventListener("click", (e) => {
+    let nombre = document.getElementById("nom").value;
+    let apellido = document.getElementById("ape").value;
+    let telefono = document.getElementById("tel").value;
+    let direccion = document.getElementById("dir").value;
+    let email = document.getElementById("email").value;
+    let id = $btnModificar.dataset.id;
+
+    
+    const datos = {
+        "id": id,
+        "nombre": nombre,
+        "apellido": apellido,
+        "telefono": telefono,
+        "direccion": direccion,
+        "email": email
+    }
+    
+    //const hayErrores = validarFormulario(datos);
+
+    const hayErrores = false;
+
+    if(!hayErrores) {
+        modificarContacto(datos);
+    }
+
+    e.preventDefault();
+});
+
+
--- js/ayax.js
+++ js/ayax.js
@@ -1,30 +1,126 @@
-/*const ayax = (options) => {
-    let {url, metodo, exito, error, datos} = options;
-    const xhttp = new XMLHttpRequest();
+const ayax = (parametros) => {
+    let {url, metodo, success, error, data} = parametros;
+    xhttp = new XMLHttpRequest();
 
     xhttp.addEventListener("readystatechange", () => {
-        if(xhttp.readyState === 4) {
+        if(xhttp.readyState === 4 ) {
             if(xhttp.status >= 200 && xhttp.status <= 300) {
-                let json = JSON.parse(xhttp.responseText);
-                exito(json);
+                //console.log(xhttp);
+                let response = JSON.parse(xhttp.responseText);
+                success(response);
             } else {
-                let mensaje = xhttp.statusText;
-                error("Error " + xhttp.status + " " + mensaje);
-            }
+                const estadoRespuesta = xhttp.status;
+                const mensajeRespuesta = xhttp.statusText;
+                error(`Error ${estadoRespuesta} ${mensajeRespuesta}`);
+            } 
         }
     });
 
     xhttp.open(metodo, url);
-    xhttp.setRequestHeader("Content-type","application/json; charset=utf-8");
-    xhttp.send(JSON.stringify(datos));
+    xhttp.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    xhttp.send(JSON.stringify(data));
+
 }
 
-const mostrarTodosLosContactos = () => {
-    ayax({
-        metodo: "get",
-        url: "http://localhost:3000/contactos",
-        exito: (res) => {console.log(res)},
-        error: (err) => {console.error(err)}
-    });
-};*/
 
+
+const mostrarContactos = () => {
+    ayax({
+        url: "http://localhost:3000/contactos",
+        metodo : "GET",
+        success: (res) => {
+            $table = document.querySelector(".table__body");
+            $fragment = document.createDocumentFragment();
+
+            res.forEach( (objeto) => {
+                $row = document.createElement("tr");
+                $row.innerHTML = "";
+                $row.innerHTML += `
+                    <td>${objeto.nombre}</td>
+                    <td>${objeto.apellido}</td>
+                    <td>${objeto.telefono}</td>
+                    <td>${objeto.direccion}</td>
+                    <td>${objeto.email}</td>
+                    <td>
+                        <button class="editar" onclick="llenarDatosDelContactoAlFormulario(event)" data-id="${objeto.id}" data-nombre="${objeto.nombre}" data-apellido="${objeto.apellido}" data-telefono="${objeto.telefono}" data-direccion="${objeto.direccion}" data-email="${objeto.email}">Editar</button>
+                        <button class="eliminar" onclick="eliminarContacto(event)" data-id="${objeto.id}">Elminar</button>
+                    </td>
+                `;
+                $fragment.appendChild($row);
+            })
+            $table.appendChild($fragment);
+        },
+
+        error: (err) => {
+            $error = document.getElementById("error");
+            $error.textContent = err;
+        }
+    });
+}
+
+const agregarContacto = (datos) => {
+
+    let {nombre, apellido, telefono, direccion, email} = datos;
+
+    ayax({
+        url: "http://localhost:3000/contactos",
+        metodo: "POST",
+        success: (res) => {
+            location.reload();
+        },
+        error: (err) => {
+            $error = document.getElementById("error");
+            $error.textContent = err;
+        },
+        data: {
+            nombre: nombre,
+            apellido: apellido,
+            telefono: telefono,
+            direccion: direccion,
+            email: email
+        }
+    })
+}
+
+const modificarContacto = (datos)  => {
+    let {id, nombre, apellido, telefono, direccion, email} = datos;
+
+    ayax({
+        url: `http://localhost:3000/contactos/${id}`,
+        metodo: "PUT",
+        success: (res) => {
+            location.reload();
+        },
+        error: (err) => {
+            $error = document.getElementById("error");
+            $error.textContent = err;
+        },
+        data: {
+            id: id,
+            nombre: nombre,
+            apellido: apellido,
+            telefono: telefono,
+            direccion: direccion,
+            email: email
+        }
+    })
+}
+
+const eliminarContacto = (e) => {
+    const id = e.target.dataset.id;
+    const isDelete = confirm("¿Está seguro de eliminar?");
+    
+    if(isDelete) {
+        ayax({
+            url: `http://localhost:3000/contactos/${id}`,
+            metodo: "DELETE",
+            success: (res) => {
+                location.reload();
+            },
+            error: (err) => {
+                $error = document.getElementById("error");
+                $error.textContent = err;
+            }
+        })
+    }
+}
